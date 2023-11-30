2a91e535b1af6463c0223eff6fd1e3dd5d3d9123
Ian Tauro <sushruth127@gmail.com>
2023-11-30 23:55:57 +0530

    Made wish list display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,20 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { WishItem } from '../shared/wishItem';
+
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	standalone: true,
+	imports: [CommonModule, RouterOutlet],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'learn-angular';
+	wishItems : WishItem[] = [
+		new WishItem('Learn Angular'),
+		new WishItem('Be Happy', true),
+		new WishItem('Play Roblox', false)
+	];
+  	title = 'World!';
 }
---
+++ src/shared/wishItem.ts
@@ -0,0 +1,3 @@
+export class WishItem {
+	constructor(public wishText : string, public completed : boolean = false) { }
+}
