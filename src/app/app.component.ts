780e716607305157e7f5f72f4594c3bca46ddc3b
Ian Tauro <sushruth127@gmail.com>
2023-12-03 22:45:47 +0530

    Added add wish form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faSquareCheck, faSquareXmark } from '@fortawesome/free-solid-svg-icons';
@@ -10,7 +11,7 @@ import { WishItem } from '../shared/wishItem';
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [CommonModule, RouterOutlet, FontAwesomeModule],
+	imports: [CommonModule, RouterOutlet, FontAwesomeModule, FormsModule],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss'
 })
@@ -27,11 +28,18 @@ export class AppComponent {
 	faSquareCheck = faSquareCheck;
 	faSquareXmark = faSquareXmark;
 
+	inputWish : string = '';
+
 	addToCompleted(index : number) {
 		this.completed.push(...this.notCompleted.splice(index, 1));
 	}
 
 	addToNotCompleted(index : number) {
 		this.notCompleted.push(...this.completed.splice(index, 1));
+	}
+
+	addWish() {
+		this.notCompleted.push(new WishItem(this.inputWish));
+		this.inputWish = '';
 	}
 }
